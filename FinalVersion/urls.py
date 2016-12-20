ed06e3113feeb438df4b793be5bafe37eea22b39
Lindokuhle2015 <lbiyase@ska.ac.za>
2016-12-20 16:43:02 +0200

    Update urls.py

--- FinalVersion/urls.py
+++ FinalVersion/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 
-from django.conf.urls import include, url ,patterns
+from django.conf.urls import include, url 
 from django.contrib import admin
 from django.conf import settings 
 from django.conf.urls.static import static
@@ -43,9 +43,9 @@ if not settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root =settings.STATIC_ROOT)
 
 #Uploading Media on to the website Setting
-if settings.DEBUG :
-    urlpatterns += patterns(
-    'django.views.static',
-    (r'^media/(?P<path>.*)',
-     'serve',
-     {'document_root': settings.MEDIA_ROOT}), )
+#if settings.DEBUG :
+   # urlpatterns += patterns(
+    #'django.views.static',
+    #(r'^media/(?P<path>.*)',
+     #'serve',
+    # {'document_root': settings.MEDIA_ROOT}), )
